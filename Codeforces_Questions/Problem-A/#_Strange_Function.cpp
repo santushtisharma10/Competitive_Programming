c1b56dab784e5364741095bf7ec7521517395516
Santushti Sharma <60578091+santushtisharma10@users.noreply.github.com>
2020-12-04 11:27:29 +0530

    Create #_Strange_Function.cpp

---
+++ Codeforces_Questions/Problem-A/#_Strange_Function.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t;
+    
+    cin>>t;
+    
+    while(t--) {
+        
+        string num;
+        
+        cin>>num;
+        
+        //int digit = log10(num);
+        
+        cout<<num.length()<<"\n";
+    }
+	return 0;
+}
